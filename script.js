0d1584242a8dee04a91ef5ca1137495f371f95a3
Abin Biju <abinbiju357@gmail.com>
2024-07-13 15:44:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function tap(){
+    document.getElementById('togglemenu').classList.toggle('display');
+}
+// var  typed= new typed("#text",{
+//     Strings: ["Frontend Developer","UI Designer"]
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
